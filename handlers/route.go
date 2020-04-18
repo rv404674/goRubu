04afbee4813ea14c32c50f3a59bc145ddfb022e4
rv404674 <rv404674@gmail.com>
2020-04-19 01:31:45 +0530

    Added a new route to shorten a url

--- handlers/route.go
+++ handlers/route.go
@@ -4,6 +4,9 @@ import (
 	"encoding/json"
 	"fmt"
 	"goRubu/middlewares"
+	service "goRubu/services"
+	"io/ioutil"
+	"log"
 	"net/http"
 
 	"github.com/gorilla/mux"
@@ -13,8 +16,13 @@ type errorResponse struct {
 	Message string
 }
 
+type inputUrl struct {
+	Url string `json:"Url"`
+}
+
 //To test a basic handler
 func Hellohandler(w http.ResponseWriter, r *http.Request) {
+	// see the imported package above
 	fmt.Fprintf(w, "Hello. Go is an Awesome Language")
 }
 
@@ -24,10 +32,31 @@ func Basichandler(w http.ResponseWriter, r *http.Request) {
 	json.NewEncoder(w).Encode(errorResponse{Message: "Used to test Basic Middleware"})
 }
 
+//created new shortened Url
+func CreateUrlHandler(w http.ResponseWriter, r *http.Request) {
+	var input inputUrl
+	reqBody, err := ioutil.ReadAll(r.Body)
+
+	if err != nil {
+		// enter url:"" pair in body
+		log.Println("Enter the url you want to shorten")
+	}
+
+	// get go object from json
+	json.Unmarshal(reqBody, &input)
+	if input.Url == "" {
+		json.NewEncoder(w).Encode(errorResponse{Message: "Enter a url"})
+	} else {
+		service.CreateShortenedUrl(input.Url)
+	}
+
+}
+
 func New() http.Handler {
 	//gorilla mux, supports addition of a middleware to a route.
 	route := mux.NewRouter()
 	route.HandleFunc("/check", Hellohandler)
+	route.HandleFunc("/shorten_url", CreateUrlHandler).Methods("POST")
 
 	//special route to use middleware
 	// when we want to access endpoints having basicMiddleware (or can be a simple Auth),
