0ef59b8828942800bc25ac4ab13507dc2c50fd25
rv404674 <rv404674@gmail.com>
2020-04-21 19:42:25 +0530

    Added a mutex Lock to enable consistency

--- services/urlCreationService.go
+++ services/urlCreationService.go
@@ -7,10 +7,20 @@ import (
 	model "goRubu/models"
 	"strconv"
 	"strings"
+	"sync"
 	"time"
 )
 
+// Race Condition - Undesirable condition where o/p of a program depends on the seq of
+// execution of go routines
+
+// To prevent this use Mutex - a locking mechanism, to ensure only one Go routine
+// is running in the CS at any point of time
+
 func CreateShortenedUrl(inputUrl string) string {
+	var m sync.Mutex
+
+	m.Lock()
 	counterVal := dao.GetCounterValue()
 	byteNumber := []byte(strconv.Itoa(counterVal))
 	tempUrl := base64.StdEncoding.EncodeToString(byteNumber)
@@ -20,9 +30,13 @@ func CreateShortenedUrl(inputUrl string) string {
 	dao.InsertInShortenedUrl(inputModel)
 
 	dao.UpdateCounter()
+	m.Unlock()
+
 	return new_url
 }
 
+//Use caching here.
+// if
 func UrlRedirection(inputUrl string) string {
 	// https://goRubu/MTAwMDE=
 	i := strings.Index(inputUrl, "Rubu/")
