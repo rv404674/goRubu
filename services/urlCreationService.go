324ef124a70b496f7c3ecfd832edc2ac10c7722f
rv404674 <rv404674@gmail.com>
2020-04-22 01:13:05 +0530

    Added Memcached Logic

--- services/urlCreationService.go
+++ services/urlCreationService.go
@@ -5,10 +5,14 @@ import (
 	"encoding/base64"
 	dao "goRubu/daos"
 	model "goRubu/models"
+	"log"
+	"os"
 	"strconv"
 	"strings"
-	"sync"
 	"time"
+
+	"github.com/joho/godotenv"
+	"github.com/rainycape/memcache"
 )
 
 // Race Condition - Undesirable condition where o/p of a program depends on the seq of
@@ -17,20 +21,49 @@ import (
 // To prevent this use Mutex - a locking mechanism, to ensure only one Go routine
 // is running in the CS at any point of time
 
-func CreateShortenedUrl(inputUrl string) string {
-	var m sync.Mutex
+var mc *memcache.Client
+var CACHE_EXPIRATION int64
+var err error
 
-	m.Lock()
+func init() {
+	if err = godotenv.Load("variables.env"); err != nil {
+		log.Fatal("Unable to load env file from urlCreationService Init", err)
+	}
+
+	// in seconds
+	CACHE_EXPIRATION, _ = strconv.ParseInt(os.Getenv("CACHE_EXPIRATION"), 0, 64)
+	mc, err = memcache.New(os.Getenv("MEMCACHED_DOMAIN"))
+	if err != nil {
+		log.Fatal("Unable to establish connection with the cache")
+	} else {
+		log.Println("Connection to Memcached Established")
+	}
+}
+
+func CreateShortenedUrl(inputUrl string) string {
+
+	// TODO handle concurrency
+
 	counterVal := dao.GetCounterValue()
 	byteNumber := []byte(strconv.Itoa(counterVal))
 	tempUrl := base64.StdEncoding.EncodeToString(byteNumber)
 
 	new_url := "https://goRubu/" + tempUrl
 	inputModel := model.UrlModel{UniqueId: counterVal, Url: inputUrl, Created_at: time.Now()}
+
+	//first update the cache with (key,val) => (new_url, inputUrl)
+	err = mc.Set(&memcache.Item{
+		Key:        new_url,
+		Value:      []byte(inputUrl),
+		Expiration: int32(CACHE_EXPIRATION),
+	})
+
+	if err != nil {
+		log.Fatal("Error in setting memcached value ", err)
+	}
 	dao.InsertInShortenedUrl(inputModel)
 
 	dao.UpdateCounter()
-	m.Unlock()
 
 	return new_url
 }
@@ -45,7 +78,16 @@ func UrlRedirection(inputUrl string) string {
 	byteNumber, _ := base64.StdEncoding.DecodeString(encodedForm)
 	UniqueId, _ := strconv.Atoi(string(byteNumber))
 
-	url := dao.GetUrl(UniqueId)
-	return url.Url
+	//try hitting the cache first
+	// stored as "https://goRubu/MTW" -> "www.google.com"
+	// url, err := mc.Get(inputUrl)
+	// if err == nil {
+	// 	log.Println("Shortened url found in cache", string(url.Value))
+	// 	return string(url.Value)
+	// } else if err != memcache.ErrCacheMiss {
+	// 	log.Fatal("Memcached error ", err)
+	// }
 
+	urlModel := dao.GetUrl(UniqueId)
+	return urlModel.Url
 }
